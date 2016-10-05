517b53ad869ebefeb14ceb9727a3bb1f3aa60247
Bassett, Rohan <RBassett@pagemasters.com.au>
2016-10-05 12:39:42 +1100

    added comments, no longer creates folders for dates with no meetings

--- fixed.go
+++ fixed.go
@@ -43,39 +43,44 @@ func getJson(url string, target interface{}) {
 }
 
 func main() {
+	// initialise defaultDay to be used if no date is specified at run-time
 	defaultDay := time.Now().Add(24 * time.Hour).Format("2006-01-02")
 
 	dayPtr := flag.String("d", defaultDay, "day to fetch, YYYY-MM-DD")
 	flag.Parse()
 
+	// fetch meeting list for specified day
 	m := Payload{}
 	getJson(fmt.Sprintf("https://api.beta.tab.com.au/v1/tab-info-service/racing/dates/%s/meetings?jurisdiction=VIC", *dayPtr), &m)
 
-	os.Mkdir(fmt.Sprintf("/Volumes/Racing/Feeds/Fixies/%s", *dayPtr), 0644)
+	if (len(m.Meetings) > 0) {
+		os.Mkdir(fmt.Sprintf("/Volumes/Racing/Feeds/Fixies/%s", *dayPtr), 0644)
 
-	for _, meeting := range m.Meetings {
-		var buffer bytes.Buffer
+		for _, meeting := range m.Meetings {
+			var buffer bytes.Buffer
 
-		buffer.WriteString(fmt.Sprintf("\n%s %s\n", meeting.Name, meeting.Date))
-		races := Races{}
+			buffer.WriteString(fmt.Sprintf("\n%s %s\n", meeting.Name, meeting.Date))
+			races := Races{}
 
-		if (meeting.Mnemonic != "") {
-			getJson(meeting.Links.Races, &races)
-		} else {
-			races.Races = meeting.Races
-		}
-
-		for _, race := range races.Races {
-			if (race.HasFixed && race.Link.Self != "") {
-				getJson(race.Link.Self, &race)
-
-				buffer.WriteString(fmt.Sprintf("Race %s - \n", getNumberWord(race.Number)))
-				getFixedOdds(race.Runners, &buffer)
+			// races are stored differently depending on the state of the meeting
+			if (meeting.Mnemonic != "") {
+				getJson(meeting.Links.Races, &races)
+			} else {
+				races.Races = meeting.Races
 			}
-		}
 
-		if err := ioutil.WriteFile(fmt.Sprintf("/Volumes/Racing/Feeds/Fixies/%s/%s.txt", meeting.Date, meeting.Name), buffer.Bytes(), 0644); err != nil {
-			panic(err)
+			for _, race := range races.Races {
+				if (race.HasFixed && race.Link.Self != "") {
+					getJson(race.Link.Self, &race)
+
+					buffer.WriteString(fmt.Sprintf("Race %s - \n", getNumberWord(race.Number)))
+					getFixedOdds(race.Runners, &buffer)
+				}
+			}
+
+			if err := ioutil.WriteFile(fmt.Sprintf("/Volumes/Racing/Feeds/Fixies/%s/%s.txt", meeting.Date, meeting.Name), buffer.Bytes(), 0644); err != nil {
+				panic(err)
+			}
 		}
 	}
 }
@@ -89,12 +94,15 @@ func getFixedOdds(runners []Runner, buffer *bytes.Buffer) {
 
 	for _, runner := range runners {
 		if ((runner.Odds.Win != 0.00) && (runner.Odds.Status == "Open")) {
+			// remove anything in brackets, eg: (Emergency)
 			r := regexp.MustCompile(" \\(.+")
 			in := []byte(runner.Name)
 			out := r.ReplaceAll(in, []byte(""))
 
-			name := strings.Title(strings.ToLower(string(out)))
+			name := strings.Title(strings.ToLower(string(out))) // convert uppercase string to lowercase, then titlecase that -- titlecasing uppercase text does not work
 
+			// lowercase any letter immediately following an apostrophe
+			// this is largely redundant since everything will be capped for comparison later on
 			r = regexp.MustCompile("'[A-Z]")
 			in = []byte(name)
 			out = r.ReplaceAllFunc(in, bytes.ToLower)
@@ -102,7 +110,6 @@ func getFixedOdds(runners []Runner, buffer *bytes.Buffer) {
 			buffer.WriteString(fmt.Sprintf("%6.2f %s\n", runner.Odds.Win, string(out)))
 		}
 	}
+	
 	buffer.WriteString(fmt.Sprintf("\n"))
-
-	return
 }
